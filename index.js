3aed61d3c5541312dcb9aad5433b9d8e45e93548
André Igor <andreigormenezes@gmail.com>
2022-07-07 23:12:15 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import {StatusCodes} from "http-status-codes";
+
+const app = express();
+const PORT = 3000;
+let users = [
+    {id: 1, name: 'André Igor', age: 20},
+    {id: 2, name: 'Douglas Silva', age: 24}
+];
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    return res.send('<h1>Hello World</h1>');
+});
+  
+app.get('/users', (req, res) => {
+    return res.send(users);
+});
+
+app.get('/users/:userId', (req, res) => {
+    const userId = req.params.userId
+    const user = users.find(user => {
+        return user.id === Number(userId);
+    });
+    return res.send(user);
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+
+    users.push(newUser);
+
+    return res.status(StatusCodes.CREATED).send(newUser);
+})
+
+app.put('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    const updatedUser = req.body;
+
+    users = users.map(user => {
+        if (Number(user.id) === userId) {
+            return updatedUser;
+        }
+
+        return user;
+    })
+
+    return res.send(updatedUser);
+});
